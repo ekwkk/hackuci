75d110cc7b89ed92e5c9e5fb0c3963d89dc1d775
Erica Kong <ericakong0331@gmail.com>
2021-01-31 19:07:23 -0800

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAIj2xP5JOCidW9NDpFf4T8LbY2ZUbEPJg',
-  authDomain: 'projects-243121.firebaseapp.com',
+  apiKey: '---',
+  authDomain: '---.firebaseapp.com',
   databaseURL: '',
-  projectId: 'projects-243121',
-  storageBucket: 'projects-243121.appspot.com',
-  messagingSenderId: '798740738273',
-  appId: '1:798740738273:ios:d34796a5eee883026607a0',
+  projectId: '---',
+  storageBucket: '---',
+  messagingSenderId: '--',
+  appId: '--',
 };
 
 if (!firebase.apps.length) {
